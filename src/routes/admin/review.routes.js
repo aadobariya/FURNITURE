b182a410815002d81b74fa4aa856187d2b45585f
Akshay Dobariya <akshaydobariya82@gmail.com>
2024-09-04 09:23:27 +0530

    furniture

---
+++ src/routes/admin/review.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const ReviewRoutes = express.Router();
+const { adminVerifyToken }  = require('../../Helpers/userVerifyToken');
+
+const {  getAllReview ,  deleteReview } = require('../../controller/admin/review.controller');
+
+
+// GET ALL REVIEW
+ReviewRoutes.get('/get-all-review' , adminVerifyToken,  getAllReview);
+
+// DELETE REVIEW
+ReviewRoutes.delete('/delete-review' , adminVerifyToken , deleteReview);
+
+
+module.exports = ReviewRoutes;
